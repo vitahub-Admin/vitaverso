b884bb78e0290af4a9d20c912fbbb3e4a74f5f0d
MaxiAlem <maerc.maxi@gmail.com>
2025-09-09 15:27:43 -0300

    version 1.1.2

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,9 +4,24 @@ import Link from "next/link";
 import SetCustomerId from "./components/SetCustomerId";
 import { Suspense } from "react";
 import Sidebar from "./components/Sidebar";
+import Cookies from "js-cookie";
 
 export default function RootLayout({ children }) {
+
+
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const customerId = urlParams.get("customerId");
+
+    if (customerId) {
+      // Guardamos cookie con el mismo nombre que el parámetro
+      Cookies.set("customerId", customerId, { expires: 30 }); // expira en 7 días
+      console.log("customerId guardado en cookie:", customerId);
+    }
+  }, []);
   return (
+    
     <html lang="en">
       <body className="h-screen flex flex-col bg-white">
         <Suspense fallback={<div>Loading...</div>}>
