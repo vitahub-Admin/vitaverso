3598c5aff57f949fd1ff10056e5c28025a5e6700
MaxiAlem <maerc.maxi@gmail.com>
2025-10-11 13:00:29 -0300

    version 2.1

--- src/app/academia-vitahub/page.jsx
+++ src/app/academia-vitahub/page.jsx
@@ -39,6 +39,43 @@ export default function AcademiaVitahubPage() {
 </div>
 
   </section> 
+  <section className="mb-16">
+
+    <h2 className="text-xl md:text-2xl text-[#1b3f7a] mb-6 font-lato">
+    Cellx
+  </h2>
+    <div className="aspect-w-16 aspect-h-9">
+      <iframe 
+        src="https://www.youtube.com/embed/fJhSET3Mxvw"
+
+        title="Cómo registrarme y tener cuenta de Afiliado en Vitahub"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        className="w-full h-64 md:h-96 rounded-lg shadow-md"
+      ></iframe>
+    </div>
+    <div className="mt-6 max-w-3xl mx-auto text-left px-4 pt-5">
+  <p className="text-gray-600 mb-4">
+  Cellx es una línea de suplementos nutracéuticos clínicamente desarrollados para optimizar la salud celular y prevenir enfermedades crónicas.
+Durante la capacitación se presentó cómo sus fórmulas actúan sobre la mitocondria, la epigenética y los mecanismos de reparación celular, con el objetivo de mejorar la energía, el metabolismo y la longevidad. Se destacó su enfoque médico-científico, basado en evidencia y con dosis efectivas de ingredientes activos.
+
+  </p>
+  <ul className="text-gray-600 list-disc list-inside mb-4 space-y-1">
+    <li><strong>Salud celular integral: </strong> Cellx aborda los pilares del envejecimiento (mitocondrias, epigenética y reparación) con un enfoque clínico.
+</li>
+    <li><strong>Prevención de enfermedades crónicas:</strong> Se mostraron aplicaciones en diabetes, cardiovasculares, SOP y cáncer.
+</li>
+    <li><strong>Formulación avanzada:</strong> Uso de tecnologías como delivery liposomal y cofactores que mejoran biodisponibilidad.
+</li>
+    <li><strong>Uso profesional:</strong>  Suplementos diseñados para acompañar protocolos médicos y optimizar la calidad de vida del paciente.
+</li>
+  </ul>
+  <p className="text-gray-500 italic text-sm">
+  Hacia el cierre se subrayó la importancia de la prescripción responsable y la integración del producto dentro de planes clínicos personalizados.
+  </p>
+</div>
+
+  </section> 
 </main>
   );
 }
